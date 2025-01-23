cfd38f3c903d8c5fb3fcb26f55ba55ca47cc9095
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-24 01:45:47 +0600

    update

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -8,10 +8,10 @@ const BlogPostDetails = async ({ params }) => {
   const post = await res.json();
 
   return (
-    <div className="flex justify-center mt-20  items-center w-1/2 mx-auto  ">
-    <div className="h-80 bg-gray-100 shadow-lg rounded-lg p-6 text-center items-center flex flex-col justify-center ">
-      <h1 className="text-2xl font-bold mb-4 text-gray-800">{post.title}</h1>
-      <p className="text-gray-700 leading-relaxed">{post.body}</p>
+    <div className="flex justify-center md:mt-20   items-center md:w-1/2 mx-auto  ">
+    <div className="md:h-80 bg-gray-100 shadow-lg mx-4 md:mx-0 rounded-lg md:p-6 p-3 text-center items-center flex flex-col justify-center ">
+      <h1 className="md:text-2xl font-bold mb-4 text-gray-800">{post.title}</h1>
+      <p className="text-gray-700 text-xs md:text-base leading-relaxed">{post.body}</p>
     </div>
   </div>
   );
