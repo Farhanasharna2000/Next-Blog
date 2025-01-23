8eabf4494c38be7165a7756c0b774c5e9c9019b2
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-23 17:16:23 +0600

    update

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const BlogPosts = ({ posts }) => {
   return (
     <div>
-      <h1>Blog Posts</h1>
+      <h2>Blog Posts</h2>
       <ul className="list-disc pl-5">
         {posts?.map((post) => (
           <li key={post.id}>
