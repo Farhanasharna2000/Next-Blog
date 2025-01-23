d7d5effbe6c57e8d16cc94860dff9de143261e36
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-23 17:11:02 +0600

    update

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,24 +1,20 @@
 import Link from "next/link";
 
-const BlogPosts = ({posts}) => {
-    return (
-        <div>
-            <h1>Blog Posts</h1>
-
-
-<ul className="list-disc pl-5">
-  {posts.map((post) => (
-    <li key={post.id}>
-      <Link href={`/posts/${post.id}`}>
-        <p>{post.title}</p>
-      </Link>
-    </li>
-  ))}
-</ul>
-        </div>
-    );
+const BlogPosts = ({ posts }) => {
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <ul className="list-disc pl-5">
+        {posts?.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BlogPosts;
-
-
