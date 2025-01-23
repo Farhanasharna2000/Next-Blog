3ee202d417c17d01048b38a7e4b4a6ff7066e3c8
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-23 19:35:35 +0600

    update homepage

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const BlogPosts = ({ posts }) => {
   return (
     <div>
-      <h1 className="md:text-4xl font-bold mb-5">Blog Posts</h1>
+      <h1 className="md:text-4xl font-bold mb-5">My Blog Posts</h1>
       <ul className="list-disc pl-5">
         {posts?.map((post) => (
           <li key={post.id}>
